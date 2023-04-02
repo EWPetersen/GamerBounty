fccf34dea45a5eda92aa14f8176e09c06533996c
EWPetersen <eric.p.mail@gmail.com>
2023-04-02 15:44:20 -0700

    more profile

--- gamebounty/pages/profile.js
+++ gamebounty/pages/profile.js
@@ -139,123 +139,6 @@ if (loading) {
     setErrorMessage('');
   }
 
-  async function handleCreate(contract) {
-    try {
-      const response = await axios.post('/api/writeContracts', {
-        gameTitle: contract.gameTitle,
-        targetPlayer: contract.targetPlayer,
-        contractConditions: contract.contractConditions,
-        expDate: contract.expDate,
-        bidAmount: contract.bidAmount,
-        acceptedby: contract.acceptedBy,
-        verifyLink: contract.verifyLink,
-        isVerified: contract.isVerified,
-        contractStatus: contract.contractStatus
-      });
-
-      console.log(response.data.data);
-      setContracts([...contracts, response.data.data]);
-      setSuccess(true);
-    } catch (error) {
-      console.error('Error creating contract', error);
-      setSuccess(false);
-    }
-  }
-
-  function CreateContractForm({ show, handleClose, handleCreate }) {
-  const [gameTitle, setgameTitle] = useState('');
-  const [targetPlayer, setTargetPlayer] = useState('');
-  const [contractConditions, setContractConditions] = useState('');
-  const [expDate, setExpDate] = useState('');
-  const [bidAmount, setBidAmount] = useState('');
-  
-    const handleFormSubmit = (event) => {
-      event.preventDefault();
-      handleCreate({
-        gameTitle,
-        targetPlayer,
-        contractConditions,
-        expDate,
-        bidAmount,
-        acceptedBy: 'create',
-        verifyLink: 'create',
-        isVerified: 'false',
-        contractStatus: 'open',
-        });
-      setShowCreateForm(false);
-    };
-
-    return (
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Create Contract</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={handleFormSubmit}>
-            <Form.Group controlId="gameTitle">
-              <Form.Label>Game Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Which game does their character live in?"
-                value={gameTitle}
-                onChange={(event) => setgameTitle(event.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="targetPlayer">
-              <Form.Label>Target Player</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="What is the name of your target?"
-                value={targetPlayer}
-                onChange={(event) => setTargetPlayer(event.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="expDate">
-              <Form.Label>Expiriation Date</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="When will this contract expire?"
-                value={expDate}
-                onChange={(event) => setExpDate(event.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="contractConditions">
-              <Form.Label>Conditions</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Are there any additional requirements? (under 100char)"
-                value={contractConditions}
-                onChange={(event) => setContractConditions(event.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="bidAmount">
-              <Form.Label>Bid Amount</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Enter bid amount"
-                value={bidAmount}
-                onChange={(event) => setBidAmount(event.target.value)}
-              />
-            </Form.Group>
-            <Form.Group>
-              ---
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Create
-            </Button>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-
-
-
   async function handleAccept() {
     try {
       const response = await axios.post('/api/updateContracts', {
@@ -333,7 +216,6 @@ const sortedContracts = sort.field
   return (
        <div className="bg-gray-900 min-h-screen text-white">
       <Navbar />
-      <CreateContractForm show={showCreateForm} handleClose={handleCloseCreateForm} handleCreate={handleCreate} />
       <Container className="bg-gray-900">
       <h1 className="text-3xl font-bold mb-8 text-center">Profile</h1>
       <div>
@@ -350,11 +232,7 @@ const sortedContracts = sort.field
                 <p>0%</p> {/* Replace with the actual rating when you have the database code */}
                 </div>
         </div>
-         <CreateContractForm
-          show={showCreateForm}
-          handleClose={handleCloseCreateForm}
-          handleCreate={handleCreate}
-        />
+        
         <Form className="mb-4">
           <Form.Group controlId="search">
             <Form.Control
@@ -502,13 +380,6 @@ const sortedContracts = sort.field
 
           </div>
         <div className="mt-4 mb-4 text-center">
-        <Button
-        variant="primary"
-        onClick={() => setShowCreateForm(!showCreateForm)}
-        className="text-xl px-8 py-2 rounded-lg" // Added classes for bigger and dynamic button
-      >
-        Create Contract
-      </Button>
         </div>
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
