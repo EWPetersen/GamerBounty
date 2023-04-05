55d171e08c9f38f90cb551d61d7af9f3141f448a
EWPetersen <eric.p.mail@gmail.com>
2023-04-05 15:36:09 -0700

    wednesday

--- gamebounty/components/ReviewContractForm.js
+++ gamebounty/components/ReviewContractForm.js
@@ -1,44 +1,53 @@
 import React from 'react';
-import styles from '../styles/ReviewContractForm.module.css';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'react-bootstrap';
 
-const ReviewContractForm = (props) => {
-  const {
-    selectedReviewContract,
-    handleApprove,
-    handleReject,
-    handleClose,
-  } = props;
-
+const ReviewContractForm = ({ selectedContract, handleApprove, handleReject, handleClose, showReviewForm }) => {
   return (
-    <div className={styles.container}>
-      {props.showReviewForm && selectedReviewContract ? (
-        <div className="review-proof-modal">
-          <h2>Review Proof</h2>
-          {/* Show contract details */}
-          {/* You can customize the contract detail display as needed */}
-          <div className="contract-details">
-            {/* Example of displaying contract details */}
-            <p>Game title: {selectedReviewContract.gameTitle.S}</p>
-            <p>Player: {selectedReviewContract.targetPlayer}</p>
-          </div>
-          {/* iframe to show content from the verifyLink attribute */}
-          {selectedReviewContract.verifyLink.S ? (
-            <iframe
-              src={selectedReviewContract.verifyLink.S}
-              title="proof"
-              width="100%"
-              height="400"
-              frameBorder="0"
-            ></iframe>
-          ) : (
-            <p>No verification link available.</p>
+    <div>
+      <Modal isOpen={showReviewForm} toggle={handleClose}>
+        <ModalHeader toggle={handleClose}>Review Proof</ModalHeader>
+        <ModalBody>
+          {selectedContract && (
+            <>
+              {/* Show contract details */}
+              {/* You can customize the contract detail display as needed */}
+              <div className="contract-details">
+                {/* Example of displaying contract details */}
+                <p>Game title: {selectedContract.gameTitle.S}</p>
+                <p>Player: {selectedContract.targetPlayer.S}</p>
+                <p>Bid amount: {selectedContract.bidAmount.N}</p>
+                <p>Requested by: {selectedContract.requestedBy.S}</p>
+                <p>Contract conditions: {selectedContract.contractConditions.S}</p>
+                <p>Status: {selectedContract.status.S}</p>
+              </div>
+              {/* iframe to show content from the verifyLink attribute */}
+              {selectedContract.verifyLink.S ? (
+                <iframe
+                  src={selectedContract.verifyLink.S}
+                  title="proof"
+                  width="100%"
+                  height="400"
+                  frameBorder="0"
+                ></iframe>
+              ) : (
+                <p>No verification link available.</p>
+              )}
+            </>
           )}
+        </ModalBody>
+        <ModalFooter>
           {/* Buttons for Approve, Reject, and Close */}
-          <button onClick={handleApprove}>Approve</button>
-          <button onClick={handleReject}>Reject</button>
-          <button onClick={handleClose}>Close</button>
-        </div>
-      ) : null}
+          <Button color="success" onClick={handleApprove}>
+            Approve
+          </Button>
+          <Button color="danger" onClick={handleReject}>
+            Reject
+          </Button>
+          <Button color="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };
