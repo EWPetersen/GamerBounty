b13984b9c812ed6b260e0b795b5579bd03b8bc20
EWPetersen <eric.p.mail@gmail.com>
2023-04-08 23:20:15 -0700

    new logic on contract entry

--- components/CreateContractForm.js
+++ components/CreateContractForm.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import { Modal, Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Modal, Form, Button, Alert } from "react-bootstrap";
 import { useSession } from "next-auth/react";
-import axios from 'axios';
-import 'tailwindcss/tailwind.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import axios from "axios";
+import "tailwindcss/tailwind.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import numeral from "numeral";
 
 function CreateContractForm({ show, handleClose }) {
-  const [gameTitle, setgameTitle] = useState('');
-  const [targetPlayer, setTargetPlayer] = useState('');
-  const [contractConditions, setContractConditions] = useState('');
-  const [expDate, setExpDate] = useState('');
+  const [gameTitle, setgameTitle] = useState("");
+  const [targetPlayer, setTargetPlayer] = useState("");
+  const [contractConditions, setContractConditions] = useState("");
+  const [expDate, setExpDate] = useState("");
   const [ingameCurrency, setIngameCurrency] = useState(true);
-  const [gameCurrencyDenom, setGameCurrencyDenom] = useState('');
-  const [gameCurrencyAmount, setGameCurrencyAmount] = useState('');
+  const [gameCurrencyDenom, setGameCurrencyDenom] = useState("");
+  const [gameCurrencyAmount, setGameCurrencyAmount] = useState("");
   const [submitStatus, setSubmitStatus] = useState(null);
   const [loading, setLoading] = useState(false);
   const [showCreateForm, setShowCreateForm] = useState(false);
@@ -37,156 +38,185 @@ function CreateContractForm({ show, handleClose }) {
     return null;
   }
 
+  const handleGameCurrencyAmountChange = (event) => {
+    const value = event.target.value;
+    const formattedValue = numeral(value).format("0,0");
+
+    setGameCurrencyAmount(formattedValue);
+  };
+
   async function handleCreate(contract) {
     try {
-      console.log('Request data:', contract); // Log the request data
-      const response = await axios.post('/api/writeContracts', {
+      const gameCurrencyAmountNumber = numeral(
+        contract.gameCurrencyAmount
+      ).value();
+
+      console.log("Request data:", contract); // Log the request data
+      const response = await axios.post("/api/writeContracts", {
         gameTitle: contract.gameTitle,
         targetPlayer: contract.targetPlayer,
         contractConditions: contract.contractConditions,
         expDate: contract.expDate,
-        ingameCurrency: contract.ingameCurrency,
         gameCurrencyDenom: contract.gameCurrencyDenom,
-        gameCurrencyAmount: contract.gameCurrencyAmount,
+        gameCurrencyAmount: String(gameCurrencyAmountNumber),
         requestedBy: contract.requestedBy,
         contractStatus: contract.contractStatus,
       });
 
       console.log(response.data.data);
       setLoading(false);
-      setSubmitStatus('success');
+      setSubmitStatus("success");
       setTimeout(() => {
         handleClose(); // Close the form after a short delay
       }, 2000);
     } catch (error) {
       setLoading(false);
-      setSubmitStatus('failure');
-      console.error('Error creating contract', error);
+      setSubmitStatus("failure");
+      console.error("Error creating contract", error);
     }
-  }    
+  }
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    console.log('handleFormSubmit called');
+    console.log("handleFormSubmit called");
     handleCreate({
       gameTitle,
       targetPlayer,
       contractConditions,
       expDate,
-      ingameCurrency,
       gameCurrencyDenom,
       gameCurrencyAmount,
       requestedBy: session.user.email,
-      contractStatus: 'open',
+      contractStatus: "open",
     });
     setShowCreateForm(false);
   };
 
-      return (
-        <>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Create Contract</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              {submitStatus === 'success' && (
-                <Alert variant="success">Contract successfully created!</Alert>
-              )}
-              {submitStatus === 'failure' && (
-                <Alert variant="danger">Failed to create contract. Please try again.</Alert>
-              )}
-              <Form onSubmit={handleFormSubmit}>
-                <Form.Group controlId="gameTitle">
-                  <Form.Label>Game: </Form.Label>
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Contract</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {submitStatus === "success" && (
+            <Alert variant="success">Contract successfully created!</Alert>
+          )}
+          {submitStatus === "failure" && (
+            <Alert variant="danger">
+              Failed to create contract. Please try again.
+            </Alert>
+          )}
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Group controlId="gameTitle">
+              <Form.Label>Game: </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Which game is your Mark playing?"
+                value={gameTitle}
+                onChange={(event) => setgameTitle(event.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="targetPlayer">
+              <Form.Label>The Mark:</Form.Label>
+              <Form.Control
+                type="text"
+                maxLength="20"
+                placeholder="Who is your target? (20char)"
+                value={targetPlayer}
+                onChange={(event) => setTargetPlayer(event.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="expDate">
+              <Form.Label>Contract Expires:</Form.Label>
+              <div className="d-flex align-items-center">
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    document.getElementById("expDate").click();
+                  }}
+                >
+                  Select Date
+                </Button>
+                <DatePicker
+                  id="expDate"
+                  name="expirationDate"
+                  selected={expDate}
+                  onChange={(date) => setExpDate(date)}
+                  className="form-control ml-2 d-none"
+                  calendarClassName="dark-calendar"
+                  minDate={new Date()}
+                  dateFormat="yyyy-MM-dd"
+                />
+              </div>
+            </Form.Group>
+            <Form.Group controlId="contractConditions">
+              <Form.Label>Additional Contract Conditions:</Form.Label>
+              <Form.Control
+                type="text"
+                maxLength="140"
+                placeholder="Are there any additional requirements? (140char)"
+                value={contractConditions}
+                onChange={(event) => setContractConditions(event.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="bidType">
+              <Form.Label>
+                USD or In-Game Currency for your opening bid?
+              </Form.Label>
+              <Form.Check
+                type="radio"
+                label="In-Game Currency"
+                checked={ingameCurrency}
+                onChange={() => setIngameCurrency(true)}
+              />
+              <Form.Check
+                type="radio"
+                label="PayPal"
+                checked={!ingameCurrency}
+                readOnly
+                disabled
+              />
+            </Form.Group>
+            {ingameCurrency && (
+              <>
+                <Form.Group controlId="gameCurrencyDenom">
+                  <Form.Label> Currency Denomination:</Form.Label>
                   <Form.Control
                     type="text"
-                    placeholder="Which game does their character live in?"
-                    value={gameTitle}
-                    onChange={(event) => setgameTitle(event.target.value)}
+                    maxLength="6"
+                    placeholder="Enter In-Game currency type  (8char)"
+                    value={gameCurrencyDenom}
+                    onChange={(event) =>
+                      setGameCurrencyDenom(event.target.value)
+                    }
                   />
                 </Form.Group>
-                <Form.Group controlId="targetPlayer">
-                  <Form.Label>Mark:</Form.Label>
+                <Form.Group controlId="gameCurrencyAmount">
+                  <Form.Label>Bid Amount:</Form.Label>
                   <Form.Control
                     type="text"
-                    placeholder="Who is your target?"
-                    value={targetPlayer}
-                    onChange={(event) => setTargetPlayer(event.target.value)}
+                    maxLength="16"
+                    placeholder="Enter bid amount (16char)"
+                    value={gameCurrencyAmount}
+                    onChange={handleGameCurrencyAmountChange}
                   />
                 </Form.Group>
-                <Form.Group controlId="expDate">
-                  <Form.Label>Expiriation:</Form.Label>
-                    <DatePicker
-                      id="expDate"
-                      name="expirationDate"
-                      selected={expDate}
-                      onChange={date => setExpDate(date)}
-                      className="form-control"
-                      calendarClassName="dark-calendar"
-                      minDate={new Date()}
-                    />
-                </Form.Group>
-                <Form.Group controlId="contractConditions">
-                  <Form.Label>Conditions:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Are there any additional requirements? (under 100char)"
-                    value={contractConditions}
-                    onChange={(event) => setContractConditions(event.target.value)}
-                  />
-                </Form.Group>
-                <Form.Group controlId="bidType">
-                  <Form.Label>Choose bid type</Form.Label>
-                  <Form.Check
-                    type="radio"
-                    label="In-Game Currency"
-                    checked={ingameCurrency}
-                    onChange={() => setIngameCurrency(true)}
-                  />
-                  <Form.Check
-                    type="radio"
-                    label="PayPal"
-                    checked={!ingameCurrency}
-                    readOnly
-                    disabled
-                  />
-                </Form.Group>
-                {ingameCurrency && (
-                  <>
-                    <Form.Group controlId="gameCurrencyDenom">
-                      <Form.Label>Game Currency Denomination:</Form.Label>
-                      <Form.Control
-                        type="text"
-                        maxLength="6"
-                        placeholder="Enter currency denomination"
-                        value={gameCurrencyDenom}
-                        onChange={(event) => setGameCurrencyDenom(event.target.value)}
-                      />
-                    </Form.Group>
-                    <Form.Group controlId="gameCurrencyAmount">
-                      <Form.Label>Game Currency Amount:</Form.Label>
-                      <Form.Control
-                        type="number"
-                        maxLength="16"
-                        placeholder="Enter currency amount"
-                        value={gameCurrencyAmount}
-                        onChange={(event) => setGameCurrencyAmount(event.target.value)}
-                      />
-                    </Form.Group>
-                  </>
-                )}
-              </Form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="success" onClick={handleFormSubmit}>
-                Create
-              </Button>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
-      );
-                }
-      
-      export default CreateContractForm;
+              </>
+            )}
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={handleFormSubmit}>
+            Create
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default CreateContractForm;
